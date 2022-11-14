da0409a688df8ba892049adec632bf78439d0e34
v3ry <v3ryfr@gmail.com>
2022-11-14 12:07:58 +0100

    first commit

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,29 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
+    Doctor jodie = new Doctor(13,"Jodie");
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public Doctor doctor(@PathVariable("number") int num) {
+        if(num == 13){
+            return jodie;
+        }else if(num >= 1 && num <= 12){
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Doctor " + num);
+        }else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + num);
+        }
+       
     }
 }
